3c90cd72f29ea915338c733a5608810d056efd30
Bakar404 <ahmedabdelsalam1202@gmail.com>
2025-08-10 19:23:13 -0400

    updated vite to point to docs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/lsat-tracker/",
   plugins: [react()],
-  build: { outDir: "dist", emptyOutDir: true },
+  build: { outDir: "../docs", emptyOutDir: true },
 });
