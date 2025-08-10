16c4cda7e24983ca9600665c52a22cd5ea0d081b
Ahmed Abdelsalam <ahmedabdelsalam1202@gmail.com>
2025-08-10 16:12:16 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,1 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  // IMPORTANT: for GitHub Pages project sites use "/<repo-name>/"
+  // Your site will be at: https://Bakar404.github.io/lsat-tracker/
+  base: "/lsat-tracker/",
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+});
